979c562590f67763f41b9f5a7d2f6ecd999345a8
littleghost <785340571@qq.com>
2019-02-03 00:31:55 +0800

    modify the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 kill -9 $(pgrep webserver_learn)
-cd ~/learnGoLang
+cd ~/learnGoLang/
 git pull https://github.com/littleghost2016/learnGoLang.git
-cd webserver_learn
+cd webserver_learn/
+chmod +x webserver_learn
 ./webserver_learn &
