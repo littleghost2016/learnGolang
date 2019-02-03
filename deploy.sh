dc5613cc67e02e4f760af334e52f236e40115c2a
littleghost <785340571@qq.com>
2019-02-03 23:13:35 +0800

    更新deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ cd ~/learnGoLang/
 chmod -x webserver_learn/webserver_learn
 git pull https://github.com/littleghost2016/learnGoLang.git
 chmod +x webserver_learn/webserver_learn
-./webserver_learn &
+webserver_learn//webserver_learn &
