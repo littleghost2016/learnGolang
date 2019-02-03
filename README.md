293cac0f62cd04c1109acafe936ab8b68ebcc4a5
littleghost <785340571@qq.com>
2019-02-03 23:52:06 +0800

    更改webserver_learn为webserver

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # learnGoLang
 GoLang学习
+
+1. 运行deployserver/deployserver后，可实现webserver的热更新
+2. GitHub中仓库已经设置WebHooks
--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 kill $(pgrep webserver_learn)
 cd ~/learnGoLang/
 # git reset --hard
-chmod -x webserver_learn/webserver_learn
+chmod -x webserver/webserver
 git pull https://github.com/littleghost2016/learnGoLang.git
-chmod +x webserver_learn/webserver_learn
-webserver_learn/webserver_learn &
+chmod +x webserver/webserver
+webserver/webserver &
