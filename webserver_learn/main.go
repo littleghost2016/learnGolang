d3c2701d2a6c3ad537b5c34a973c158d8ad88dd7
littleghost <785340571@qq.com>
2019-02-03 23:46:41 +0800

    更新webserver_learn至第5个版本，测试deployserver

--- webserver_learn/main.go
+++ webserver_learn/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1>This is my 4th page</h1>")
+	io.WriteString(w, "<h1>This is my 5th page</h1>")
 }
 
 func main() {
