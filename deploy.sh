01708dfc668a4bc8d96f350bcb5ac50f3dc98134
littleghost <785340571@qq.com>
2019-02-03 23:01:46 +0800

    change the webserver_learn to the third version

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 kill $(pgrep webserver_learn)
 cd ~/learnGoLang/
 # git reset --hard
+chmod -x webserver_learn/webserver_learn
 git pull https://github.com/littleghost2016/learnGoLang.git
-cd webserver_learn/
-chmod +x webserver_learn
+chmod +x webserver_learn/webserver_learn
 ./webserver_learn &
--- webserver_learn/main.go
+++ webserver_learn/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1>This is my second page</h1>")
+	io.WriteString(w, "<h1>This is my third page</h1>")
 }
 
 func main() {
