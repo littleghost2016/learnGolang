5034c6d9d5db6dc3da8cb08b73c10d82b786e165
littleghost2016 <785340571@qq.com>
2020-02-20 22:48:24 +0800

    remove the extra files

--- learn_devops/deploy.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-kill $(pgrep webserver_learn)
-cd ~/learnGolang/learn_devops
-# git reset --hard
-chmod -x webserver/webserver
-git pull https://github.com/littleghost2016/learnGoLang.git
-chmod +x webserver/webserver
-webserver/webserver &
--- learn_videoserver/go.mod
+++
@@ -1,8 +0,0 @@
-module learn_videoserver
-
-go 1.13
-
-require (
-	github.com/go-sql-driver/mysql v1.5.0
-	github.com/julienschmidt/httprouter v1.3.0
-)
